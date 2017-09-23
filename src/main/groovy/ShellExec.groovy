70ebe0b2c4adbe0c47478b6c7d237d3cded1240a
kindler <kindler@swissquant.com>
2017-09-23 12:11:32 +0200

    refactoring

--- src/main/groovy/ShellExec.groovy
+++ src/main/groovy/ShellExec.groovy
@@ -32,32 +32,4 @@ class ShellExec {
         }
     }
 
-    @Deprecated
-    static byte[] exec(String command) {
-        def cmd = ""
-
-        if (SystemUtils.IS_OS_WINDOWS) {
-            cmd = "cmd /C $command"
-        } else {
-            cmd = "$command"
-        }
-
-        logger.info("execute: $cmd")
-        def process = cmd.execute()
-        def out = new ByteArrayOutputStream()
-        def err = new ByteArrayOutputStream()
-        process.waitForProcessOutput(out, err)
-
-        if (process.exitValue() == 0) {
-            def stdOut = out.toString()
-            logger.info("${process.exitValue()}: ${stdOut.substring(0, Math.min(stdOut.length(), 255))}")
-            return out.toByteArray()
-        } else {
-            def stdOut = out.toString()
-            def errOut = err.toString()
-            logger.error("${process.exitValue()}: ${stdOut}\n$errOut")
-            return new byte[0]
-        }
-
-    }
 }
